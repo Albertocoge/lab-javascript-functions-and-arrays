6596627eef77f9241c276cd2779138e73725efa1
Alberto Codoñer Gea <alberto.cg2197@gmail.com>
2023-12-19 17:05:46 +0100

    Solved lab

--- src/functions-and-arrays.js
+++ src/functions-and-arrays.js
@@ -1,23 +1,49 @@
 // Iteration #1: Find the maximum
-function maxOfTwoNumbers() {}
+function maxOfTwoNumbers(num1,num2) {
+  if (num1 > num2) {
+    return num1;
+  }
+  else {
+  return num2; 
+}
+}
 
 
 
 // Iteration #2: Find longest word
 const words = ['mystery', 'brother', 'aviator', 'crocodile', 'pearl', 'orchard', 'crackpot'];
 
-function findLongestWord() {}
+function findLongestWord(words) {
+  let word = ""
+  for ( let i = 0; i < words.length; i++) {
+    if (word.length < words[i].length) {
+      word = words[i]
+    }
+  }
+  return word
+}
+console.log(findLongestWord(words))
+
 
 
 
 // Iteration #3: Calculate the sum
 const numbers = [6, 12, 1, 18, 13, 16, 2, 1, 8, 10];
 
-function sumNumbers() {}
+function sumNumbers(numbers) {
+  let total = 0
+  for (let i = 0; i < numbers.length; i++) {
+    total = total + numbers [i]
+    
+
+  }
+  return total
+}
 
 
 
-// Iteration #3.1 Bonus:
+// Iteration #3.2 Bonus:
+//Iteration #3.1: Sum numbers
 function sum() {}
 
 
@@ -26,7 +52,15 @@ function sum() {}
 // Level 1: Array of numbers
 const numbersAvg = [2, 6, 9, 10, 7, 4, 1, 9];
 
-function averageNumbers() {}
+function averageNumbers(numbersAvg) {
+  
+  let sum = sumNumbers(numbers);
+  const average = sum / numbers.length;
+  return average;
+}
+
+  
+
 
 
 // Level 2: Array of strings
